042538c6dc4ec8a89ffc308f9485ddf98b100217
Dony <aquinosilvadony1409@gmail.com>
2026-04-10 18:54:42 -0300

    Mexi no JS frontend, para que ele monitore os inputs e envie cada alteração deles para o localStorage. Por enquanto só tá funcionando com os 2 inputs padrôes. Para a próxima atualização: Salvar no localStorage também os dados dos inputs adicionados pelo usuário pelo botão. Fazer também com que os dados do localStorage sejam exibidos no value dos inputs. Por enquanto só funciona nos inputs de quantidade, adicionar também para os de select. Para processos finais, ao enviar o formulário (criando a cesta padrão) deve-se limpar os dados no localStorage.

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,4 @@
+import { get } from 'core-js/core/dict';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
@@ -8,4 +9,20 @@ const addCampo = document.querySelector('#addCampo');
 addCampo.addEventListener('click', () => {
     const htmlDoTemplate = templateSelect.innerHTML;
     container.insertAdjacentHTML('beforeend', htmlDoTemplate);
-})
+});
+
+const qtdInput = document.querySelectorAll('.qtdInput');
+
+qtdInput.forEach((e, index) => {
+    e.addEventListener('keyup', event => {
+        localStorage.setItem(`Input${index}`, `${event.target.value}`);
+    });    
+});
+
+window.addEventListener('load', () => {
+    qtdInput.forEach((e, index) => {
+        qtdInput[index].value = localStorage.getItem(`Input${index}`); 
+    });
+});
+
+
